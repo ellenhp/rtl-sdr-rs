ca23fd2dac81a6b4b8cd38cffadf01e25eb3f8a6
Bastian Bloessl <mail@bastibl.net>
2023-01-17 09:41:54 +0100

    warnings

--- src/rtlsdr.rs
+++ src/rtlsdr.rs
@@ -36,7 +36,7 @@ pub struct RtlSdr {
     corr: i32, // PPM
     force_bt: bool,
     force_ds: bool,
-    fir: [i32; FIR_LEN],
+    _fir: [i32; FIR_LEN],
 }
 
 impl RtlSdr {
@@ -55,7 +55,7 @@ impl RtlSdr {
             corr: 0,
             force_bt: false,
             force_ds: false,
-            fir: *DEFAULT_FIR,
+            _fir: *DEFAULT_FIR,
         }
     }
 
